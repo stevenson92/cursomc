0e16f327e8427ae6b23abaf386c825e52adb6ed4
Stevenson <smpengenharia@hotmail.com>
2021-10-14 10:54:02 +0100

    It's testing the first domain class - Category

--- src/main/java/com/stevenson/cursomc/resources/CategoryResource.java
+++ src/main/java/com/stevenson/cursomc/resources/CategoryResource.java
@@ -1,16 +1,30 @@
 package com.stevenson.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.stevenson.cursomc.domain.Category;
 
 @RestController
 @RequestMapping(value="/categories")
 public class CategoryResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String toList() {
-		return "REST está funcionando";
+	public List<Category> toList() {
+		
+		Category cat1 = new Category(1, "Informática");
+		Category cat2 = new Category(2, "Escritório");
+		
+		List<Category> categoryList = new ArrayList(); 
+		
+		categoryList.add(cat1);
+		categoryList.add(cat2);
+		
+		return categoryList;
 	}
 	
 	
