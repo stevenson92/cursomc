234291673add51ae680bece7db7346b6e63b1fba
Stevenson <smpengenharia@hotmail.com>
2021-10-14 13:15:47 +0100

    Created a instatiation operation

--- src/main/java/com/stevenson/cursomc/CursomcApplication.java
+++ src/main/java/com/stevenson/cursomc/CursomcApplication.java
@@ -1,13 +1,33 @@
 package com.stevenson.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.stevenson.cursomc.domain.Category;
+import com.stevenson.cursomc.repositories.CategoryRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Informática");
+		Category cat2 = new Category(null, "Escritório");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1,cat2));
+		
 	}
 
 }
